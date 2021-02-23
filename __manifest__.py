8154e0472e5a3933af3639393b4bf12014712601
pilotrick <56171872+pilotrick@users.noreply.github.com>
2021-02-23 15:16:43 -0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    # Theme information
+    'name': 'Portal Extended',
+    'category': 'Website',
+    'version': '1.0',
+    'description': """""",
+
+    # Dependencies
+    'depends': [
+        'sale'
+    ],
+
+    # Views
+    'data': [
+        'templates/template.xml',
+    ],
+
+    # Technical
+    'installable': True,
+    'application': True,
+}
